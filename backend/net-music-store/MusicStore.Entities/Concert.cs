d632bdd353c3a62614cd1b5c9d96dbbd3b657bb6
abelsaul <abelmiraval@gmail.com>
2022-09-10 18:20:38 -0500

    Add layer Entities

---
+++ backend/net-music-store/MusicStore.Entities/Concert.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MusicStore.Entities
+{
+    public class Concert
+    {
+        public int Id { get; set; }
+
+        [StringLength(100)]
+        public string Title { get; set; }
+        
+        [StringLength(500)]
+        public string Description { get; set; }
+        
+        public DateTime DateEvent { get; set; }
+        
+        public int TicketsQuantity { get; set; }
+        
+        public decimal UnitPrice { get; set; }
+        
+        public string? ImageUrl { get; set; }
+        
+        [StringLength(50)]
+        public string? Place { get; set; }
+        
+        public int GenreId { get; set; }
+        
+        public Genre Genre { get; set; }
+        
+        public bool Finalized { get; set; }
+        
+        public bool Status { get; set; }
+    }
+}
---
+++ backend/net-music-store/MusicStore.Entities/Genre.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MusicStore.Entities
+{
+    public class Genre
+    {
+        public int Id { get; set; }
+
+        [StringLength(100)]
+        public string Description { get; set; }
+
+        public bool Status { get; set; }
+    }
+}
