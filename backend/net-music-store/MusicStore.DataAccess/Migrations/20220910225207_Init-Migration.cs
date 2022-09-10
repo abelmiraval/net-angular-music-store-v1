d4c26a6d493e37daa7cf474658877f1b0f9b0a64
abelsaul <abelmiraval@gmail.com>
2022-09-10 18:22:44 -0500

    Add layer DataAccess

---
+++ backend/net-music-store/MusicStore.DataAccess/Migrations/20220910225207_Init-Migration.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MusicStore.DataAccess.Migrations
+{
+    public partial class InitMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Genres",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Description = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    Status = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Genres", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Concerts",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Title = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(500)", maxLength: 500, nullable: false),
+                    DateEvent = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    TicketsQuantity = table.Column<int>(type: "int", nullable: false),
+                    UnitPrice = table.Column<decimal>(type: "decimal(5,2)", precision: 5, scale: 2, nullable: false),
+                    ImageUrl = table.Column<string>(type: "varchar(500)", unicode: false, maxLength: 500, nullable: true),
+                    Place = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: true),
+                    GenreId = table.Column<int>(type: "int", nullable: false),
+                    Finalized = table.Column<bool>(type: "bit", nullable: false),
+                    Status = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Concerts", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Concerts_Genres_GenreId",
+                        column: x => x.GenreId,
+                        principalTable: "Genres",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Concerts_GenreId",
+                table: "Concerts",
+                column: "GenreId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Concerts");
+
+            migrationBuilder.DropTable(
+                name: "Genres");
+        }
+    }
+}
---
+++ backend/net-music-store/MusicStore.DataAccess/MusicStoreDbContext.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MusicStore.Entities;
+
+namespace MusicStore.DataAccess
+{
+    public class MusicStoreDbContext : DbContext
+    {
+        public MusicStoreDbContext()
+        {
+
+        }
+
+        public MusicStoreDbContext(DbContextOptions<MusicStoreDbContext> options)
+            :base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Concert>()
+                .Property(e => e.UnitPrice)
+                .HasPrecision(5, 2);
+
+            modelBuilder.Entity<Concert>()
+                .Property(e => e.ImageUrl)
+                .HasMaxLength(500)
+                .IsUnicode(false);
+        }
+
+        public DbSet<Concert> Concerts { get; set; }
+        public DbSet<Genre> Genres { get; set; }
+
+
+    }
+}
