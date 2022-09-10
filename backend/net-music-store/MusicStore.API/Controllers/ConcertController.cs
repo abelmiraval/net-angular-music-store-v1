9ca8eaa7e92a462a7e5888a8e9804a75b1300a7e
abelsaul <abelmiraval@gmail.com>
2022-09-10 18:23:03 -0500

    Add layer API

---
+++ backend/net-music-store/MusicStore.API/Controllers/ConcertController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MusicStore.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ConcertController : ControllerBase
+    {
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("Hola Mundo");
+        }
+    }
+}
--- backend/net-music-store/MusicStore.API/Program.cs
+++ backend/net-music-store/MusicStore.API/Program.cs
@@ -1,7 +1,13 @@
+using Microsoft.EntityFrameworkCore;
+using MusicStore.DataAccess;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+builder.Services.AddDbContext<MusicStoreDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("MusicStoreDB"));
+});
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
