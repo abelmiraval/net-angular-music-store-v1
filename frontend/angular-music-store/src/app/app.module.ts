61b984f6c85c9d40a0b7bb94f493542e53d57da9
abelsaul <abelmiraval@gmail.com>
2022-10-11 10:25:26 -0500

    Add component container, header, footer

--- frontend/angular-music-store/src/app/app.module.ts
+++ frontend/angular-music-store/src/app/app.module.ts
@@ -3,11 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FooterComponent } from './commons/components/container/components/footer/footer.component';
+import { HeaderComponent } from './commons/components/container/components/header/header.component';
+import { ContainerComponent } from './commons/components/container/container.component';
 
 @NgModule({
-	declarations: [AppComponent],
+	declarations: [AppComponent, FooterComponent, HeaderComponent, ContainerComponent],
 	imports: [BrowserModule, BrowserAnimationsModule],
 	providers: [],
 	bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule { }
