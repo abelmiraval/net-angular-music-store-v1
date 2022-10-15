1af85cd33bb7bbb6e1025fd78bb52ee852cbfec7
abelsaul <abelmiraval@gmail.com>
2022-10-15 11:15:31 -0500

    Add life cycle of a component

--- frontend/angular-music-store/src/app/app.component.ts
+++ frontend/angular-music-store/src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, OnInit } from '@angular/core';
 
 @Component({
 	selector: 'app-root',
 	templateUrl: './app.component.html',
 	styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit, AfterViewInit {
 	title = 'angular-music-store';
+	constructor() {
+		console.log('--constructor--');
+
+		this.title = "Hello world"
+	}
+
+	ngOnInit(): void {
+		console.log('---ngOnInit ---')
+	}
+
+	ngAfterViewInit(): void {
+		console.log('---ngAfterViewInit ---')
+	}
 }
