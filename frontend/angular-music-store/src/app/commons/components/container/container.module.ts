29d905c2c32659b168e22faddd95d91603161801
abelsaul <abelmiraval@gmail.com>
2022-10-17 12:09:47 -0500

    Add router outlet

--- frontend/angular-music-store/src/app/commons/components/container/container.module.ts
+++ frontend/angular-music-store/src/app/commons/components/container/container.module.ts
@@ -1,13 +1,13 @@
 import { NgModule } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { RouterModule } from '@angular/router';
 import { FooterComponent } from './components/footer/footer.component';
 import { HeaderComponent } from './components/header/header.component';
 import { ContainerComponent } from './container.component';
-import { MatButtonModule } from '@angular/material/button';
-
 
 @NgModule({
-    declarations: [ContainerComponent, HeaderComponent, FooterComponent],
-    imports: [MatButtonModule],
-    exports: [ContainerComponent],
+	declarations: [ContainerComponent, HeaderComponent, FooterComponent],
+	imports: [MatButtonModule, RouterModule],
+	exports: [ContainerComponent]
 })
-export class ContainerModule { }
+export class ContainerModule {}
