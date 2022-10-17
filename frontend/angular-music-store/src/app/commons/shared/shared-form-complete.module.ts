45be6d7ad25b6bbfb9ca88fee30c82b70c7c3fb1
abelsaul <abelmiraval@gmail.com>
2022-10-17 12:00:21 -0500

    Refactor module shared

--- frontend/angular-music-store/src/app/commons/shared/shared-form-complete.module.ts
+++ frontend/angular-music-store/src/app/commons/shared/shared-form-complete.module.ts
@@ -2,13 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common'
 import { ReactiveFormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
-import { MatCardModule } from '@angular/material/card'
 import { MatNativeDateModule } from '@angular/material/core';
 import { MatDatepickerModule } from '@angular/material/datepicker'
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon'
 import { MatInputModule } from '@angular/material/input';
 import { MatSelectModule } from '@angular/material/select'
+import { MatCardModule } from '@angular/material/card';
 
 @NgModule({
     exports: [
